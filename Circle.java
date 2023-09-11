c166dd72386ea8c8ecfcee2dd3ddca7bae5a814f
Ajayfitrezo <kajay120124@gmail.com>
2023-09-11 06:06:05 -0700

    (program for calculating the area of circle and perimeter)

---
+++ Circle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Circle
+{
+   public float area(r){
+    float Pi=3.14;
+     float area=Pi*r*r;
+     return area;
+       }
+    public int perimeter(r){
+        float Pi=3.14;
+        float perimeter=2*Pi*r;
+        return perimeter;
+    }
+    public static void main(String args []){
+        Scanner s1=new Scanner(System.in)
+        System.out.print("enter the Radius:");
+        S1.nextfloat
+    }
+}

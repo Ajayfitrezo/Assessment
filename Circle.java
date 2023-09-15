18b2a6580eae59110397f326d46c01714408e1a3
Ajayfitrezo <104978891+Ajayfitrezo@users.noreply.github.com>
2023-09-15 19:17:10 +0530

    circle concept in java

--- Circle.java
+++ Circle.java
@@ -1,19 +1,24 @@
 import java.util.*;
 class Circle
 {
-   public float area(int r){
-    float Pi=3.14;
-     float area=Pi*r*r;
+   public double area(int r){
+    double Pi=3.14;
+   double area=Pi*r*r;
      return area;
        }
-    public int perimeter (int r){
-        float Pi=3.14;
-        float perimeter=2*Pi*r;
+    public double  perimeter (int r){
+       double Pi=3.14;
+       double perimeter=2*Pi*r;
         return perimeter;
     }
     public static void main(String args []){
-        Scanner s1=new Scanner(System.in)
-        System.out.print("enter the Radius:"+12);
+        Scanner s1=new Scanner(System.in);
+         System.out.print("enter the Radius:");
+        int r1=s1.nextInt();
+        Circle c1=new Circle();
+        System.out.println("The area of circle is:"+c1.area(r1));
+        System.out.println("The perimeter of circle is:"+c1.perimeter(r1));
+       
         
     }
 }
